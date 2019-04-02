6d065c7647f7b6c1db56fdab60a1f133fd8824cd
Henok Nuguse <h.nuguse@nipo.com>
2019-04-02 16:08:01 +0200

    Update samples for test

--- dev.js
+++ dev.js
@@ -1,8 +1,6 @@
 var yamlwriter = require('./yamlw');
 
-//var set ="image.repository='janisonhackregistry.azurecr.io/$(Build.Repository.Name)',image.tag='$(Build.BuildNumber)',outputs.modelfolder='/mnt/azure/$(Build.BuildNumber)',env.BLOB_STORAGE_ACCOUNT='$(BLOB_STORAGE_ACCOUNT)',env.BLOB_STORAGE_KEY='$(BLOB_STORAGE_KEY)', env.BLOB_STORAGE_CONTAINER='$(BLOB_STORAGE_CONTAINER)',env.BLOB_STORAGE_CSV_FOLDER='$(BLOB_STORAGE_CSV_FOLDER)',env.TENANTID='$(TENANTID)'";
+var set="A-Team"
+var result = yamlwriter("samples/sample.yml", true, set, false, true);
 
-var set="ver='someting'"
-
-var result = yamlwriter("samples/config.json", true, set, true);
 console.log(result);
